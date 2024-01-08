6f11a5e737fa2e9e87dc129faec3212ffa00e735
LeilanNaeimi <leilan.n@gmail.com>
2024-01-08 20:22:44 +0300

    add css

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,47 @@
 import React from 'react';
 
-function App() {
+export default function App() {
   return (
     <>
-      <h3>flashcard</h3>
+      <Flashcards />
     </>
   );
 }
 
-export default App;
+const questions = [
+  {
+    id: 10,
+    question: `What is the result of the following expression?
+                console.log(5 + "5");`,
+    answer: `"55"`,
+  },
+  {
+    id: 11,
+    question: `What is the difference between 'let', 'const', and 'var' when declaring variables? `,
+    answer: `- 'var' is function-scoped and can be redeclared.
+    - 'let' is block-scoped and can be reassigned.
+    - 'const' is block-scoped and cannot be reassigned.`,
+  },
+  {
+    id: 12,
+    question: `What does the '===' operator do in JavaScript?`,
+    answer: `The '===' operator checks for both value and type equality.`,
+  },
+  {
+    id: 13,
+    question: ` How do you create an array in JavaScript?`,
+    answer: `var myArray = [1, 2, 3, 4, 5];`,
+  },
+];
+
+function Flashcards() {
+  return (
+    <div className="flashcards">
+      {questions.map(question => (
+        <div key={question.id}>
+          <p>{question.question}</p>
+        </div>
+      ))}
+    </div>
+  );
+}
